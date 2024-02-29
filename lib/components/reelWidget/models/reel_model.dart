d6fffce4074c234744ab6b5d15c691b9e6f1e738
imakki <ankit.escalate360@gmail.com>
2024-02-29 17:52:29 +0530

    initial commit for reel viewer and project setup

---
+++ lib/components/reelWidget/models/reel_model.dart
@@ -0,0 +1,21 @@
+import 'package:reels/components/reelWidget/reels_main.dart';
+
+class ReelModel {
+  final String? id;
+  final String url;
+  final bool isLiked;
+  final int likeCount;
+  final String userName;
+  final String? profileUrl;
+  final String? reelDescription;
+  final String? musicName;
+  final List<ReelCommentModel>? commentList;
+  ReelModel(this.url, this.userName,
+      {this.id,
+      this.isLiked = false,
+      this.likeCount = 0,
+      this.profileUrl,
+      this.reelDescription,
+      this.musicName,
+      this.commentList});
+}
---
+++ lib/components/reelWidget/reels_main.dart
@@ -0,0 +1,5 @@
+library reels_viewer;
+
+export '/components/reelWidget/reels_viewer.dart';
+export '/components/reelWidget/models/reel_model.dart';
+export '/components/reelWidget/models/reel_comment_model.dart';
